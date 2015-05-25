2aeb9deee1774d70be33884cbf385df00219f053
Juha Tauriainen <juha@bin.fi>
2015-05-25 16:32:57 +0300

    Added more rooms

--- public/js/build.js
+++ public/js/build.js
@@ -260,6 +260,26 @@
               name: "Room #666",
               messages: 0,
               icon: 'http://i.imgur.com/CfmbeXib.jpg'
+            }, {
+              room_id: 4,
+              name: "Politics",
+              messages: 0,
+              icon: 'http://i.imgur.com/JxtD1vcb.jpg'
+            }, {
+              room_id: 5,
+              name: "Pictures of cats",
+              messages: 0,
+              icon: 'http://i.imgur.com/RaKwQD7b.jpg'
+            }, {
+              room_id: 6,
+              name: "Best of Youtube",
+              messages: 0,
+              icon: 'http://i.imgur.com/aaVkYvxb.png'
+            }, {
+              room_id: 7,
+              name: "Usersub",
+              messages: 0,
+              icon: 'http://i.imgur.com/YQwZUiJb.gif'
             }
           ];
           return $scope.setActiveGroup($scope.groups[0]);
