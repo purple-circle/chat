33b8cca947e15ef62c29d108c91583fb583e8d2b
Juha Tauriainen <juha@bin.fi>
2015-05-28 16:32:19 +0300

    Pass build time to static files

--- coffeescript/routes/index.coffee
+++ coffeescript/routes/index.coffee
@@ -4,8 +4,14 @@ express = require("express")
 router = express.Router()
 
 router.get "/", (req, res) ->
+  fs = require("fs")
+  buildStats = fs.statSync("public/js/build.js")
+
+  buildModified = new Date(buildStats.mtime).getTime()
+
   res.render "index",
     sid: req.sessionID
+    buildModified: buildModified
 
 
 router.get "*", (req, res) ->
--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,13 @@
   router = express.Router();
 
   router.get("/", function(req, res) {
+    var buildModified, buildStats, fs;
+    fs = require("fs");
+    buildStats = fs.statSync("public/js/build.js");
+    buildModified = new Date(buildStats.mtime).getTime();
     return res.render("index", {
-      sid: req.sessionID
+      sid: req.sessionID,
+      buildModified: buildModified
     });
   });
 
