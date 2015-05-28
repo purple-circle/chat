b938988c9f734cb71d2ec19719f615a9e5f6b317
Juha Tauriainen <juha@bin.fi>
2015-05-28 20:17:04 +0300

    Css build date added

--- coffeescript/routes/index.coffee
+++ coffeescript/routes/index.coffee
@@ -5,13 +5,17 @@ router = express.Router()
 
 router.get "/", (req, res) ->
   fs = require("fs")
-  buildStats = fs.statSync("public/js/build.js")
+  jsBuildStats = fs.statSync("public/js/build.js")
+  jsBuildModified = new Date(jsBuildStats.mtime).getTime()
 
-  buildModified = new Date(buildStats.mtime).getTime()
+  cssBuildStats = fs.statSync("public/css/chat.css")
+  cssBuildModified = new Date(cssBuildStats.mtime).getTime()
 
-  res.render "index",
+  res.render "index", {
     sid: req.sessionID
-    buildModified: buildModified
+    jsBuildModified
+    cssBuildModified
+  }
 
 
 router.get "*", (req, res) ->
--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,16 @@
   router = express.Router();
 
   router.get("/", function(req, res) {
-    var buildModified, buildStats, fs;
+    var cssBuildModified, cssBuildStats, fs, jsBuildModified, jsBuildStats;
     fs = require("fs");
-    buildStats = fs.statSync("public/js/build.js");
-    buildModified = new Date(buildStats.mtime).getTime();
+    jsBuildStats = fs.statSync("public/js/build.js");
+    jsBuildModified = new Date(jsBuildStats.mtime).getTime();
+    cssBuildStats = fs.statSync("public/css/chat.css");
+    cssBuildModified = new Date(cssBuildStats.mtime).getTime();
     return res.render("index", {
       sid: req.sessionID,
-      buildModified: buildModified
+      jsBuildModified: jsBuildModified,
+      cssBuildModified: cssBuildModified
     });
   });
 
