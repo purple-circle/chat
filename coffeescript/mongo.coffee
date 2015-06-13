dd671071a9693a7d7431e5503bfe71064bcb5709
Juha Tauriainen <juha@bin.fi>
2015-06-13 11:55:39 +0300

    Preparing for signups

--- coffeescript/mongo.coffee
+++ coffeescript/mongo.coffee
@@ -1,5 +1,14 @@
 module.exports = (settings) ->
   mongoose = require('mongoose')
+  passportLocalMongoose = require('passport-local-mongoose')
+
+  getRandomName = ->
+     num = Math.ceil(Math.random() * 1000)
+     "Anonymous Monkeyhandler #{num}"
+
+  getRandomUserName = ->
+     num = Math.ceil(Math.random() * 1000)
+     "anonymous.manbearpig.#{num}"
 
   chatMessageSchema = mongoose.Schema {
     chat_id: 'String'
@@ -74,6 +83,81 @@ module.exports = (settings) ->
     created_at: { type: Date, default: Date.now }
   }
 
+  userSchema = mongoose.Schema {
+    name: { type: String, trim: true, default: getRandomName }
+    username: { type: String, lowercase: true, trim: true, default: getRandomUserName }
+    email: { type: String, lowercase: true, trim: true }
+    password: 'String'
+    gender: { type: String, lowercase: true, trim: true, default: "doge" }
+    bio: 'String'
+    original_bio: 'String'
+    birthday: 'Date'
+    facebook_id: 'String'
+    google_id: 'String'
+    instagram_id: 'String'
+    picture_url: 'String'
+    cover_url: 'String'
+    fanpage_id: 'ObjectId'
+    metadata: 'Object'
+    created: { type: Date, default: Date.now }
+    show_birthday: { type: Boolean, default: true }
+    show_bio: { type: Boolean, default: true }
+    show_gender: { type: Boolean, default: true }
+    hidden: { type: Boolean, default: false }
+    random: {type: [Number], index: '2d', default: -> return [Math.random(), Math.random()]}
+  }
+
+  facebookUserSchema = mongoose.Schema {
+    id: 'String'
+    user_id: 'String'
+    name: 'String'
+    first_name: 'String'
+    middle_name: 'String'
+    last_name: 'String'
+    username: { type: String, lowercase: true, trim: true }
+    url: 'String'
+    gender: 'String'
+    email: 'String'
+    emails: { type: Array }
+    quotes: 'String'
+    bio: 'String'
+    birthday: 'Date'
+    locale: 'String'
+    timezone: 'String'
+    verified: 'String'
+    metadata: 'Object'
+    accessToken: 'String'
+    created: { type: Date, default: Date.now }
+  }
+
+  googleUserSchema = mongoose.Schema {
+    id: 'String'
+    user_id: 'String'
+    name: 'String'
+    first_name: 'String'
+    last_name: 'String'
+    emails: { type: Array }
+    metadata: 'Object'
+    identifier: 'String'
+    created: { type: Date, default: Date.now }
+  }
+
+  instagramUserSchema = mongoose.Schema {
+    id: 'String'
+    user_id: 'String'
+    name: 'String'
+    first_name: 'String'
+    last_name: 'String'
+    bio: 'String'
+    website: 'String'
+    profile_picture: 'String'
+    metadata: 'Object'
+    accessToken: 'String'
+    created: { type: Date, default: Date.now }
+  }
+
+  userSchema.plugin(passportLocalMongoose)
+
   mongoose.model 'chat_messages', chatMessageSchema
   mongoose.model 'topics', topicSchema
   mongoose.model 'rooms', roomSchema
@@ -81,6 +165,10 @@ module.exports = (settings) ->
   mongoose.model 'twitter_tags', twitterTagSchema
   mongoose.model 'open_graph_tags', openGraphTagSchema
   mongoose.model 'api_logs', apiLogSchema
+  mongoose.model 'users', userSchema
+  mongoose.model 'facebook_user_data', facebookUserSchema
+  mongoose.model 'instagram_user_data', instagramUserSchema
+  mongoose.model 'google_user_data', googleUserSchema
 
 
   db = mongoose.connection
--- mongo.js
+++ mongo.js
@@ -1,7 +1,18 @@
 (function() {
   module.exports = function(settings) {
-    var apiLogSchema, chatMessageSchema, db, imgurSchema, mongoose, openGraphTagSchema, roomSchema, topicSchema, twitterTagSchema;
+    var apiLogSchema, chatMessageSchema, db, facebookUserSchema, getRandomName, getRandomUserName, googleUserSchema, imgurSchema, instagramUserSchema, mongoose, openGraphTagSchema, passportLocalMongoose, roomSchema, topicSchema, twitterTagSchema, userSchema;
     mongoose = require('mongoose');
+    passportLocalMongoose = require('passport-local-mongoose');
+    getRandomName = function() {
+      var num;
+      num = Math.ceil(Math.random() * 1000);
+      return "Anonymous Monkeyhandler " + num;
+    };
+    getRandomUserName = function() {
+      var num;
+      num = Math.ceil(Math.random() * 1000);
+      return "anonymous.manbearpig." + num;
+    };
     chatMessageSchema = mongoose.Schema({
       chat_id: 'String',
       room_id: 'ObjectId',
@@ -89,6 +100,132 @@
         "default": Date.now
       }
     });
+    userSchema = mongoose.Schema({
+      name: {
+        type: String,
+        trim: true,
+        "default": getRandomName
+      },
+      username: {
+        type: String,
+        lowercase: true,
+        trim: true,
+        "default": getRandomUserName
+      },
+      email: {
+        type: String,
+        lowercase: true,
+        trim: true
+      },
+      password: 'String',
+      gender: {
+        type: String,
+        lowercase: true,
+        trim: true,
+        "default": "doge"
+      },
+      bio: 'String',
+      original_bio: 'String',
+      birthday: 'Date',
+      facebook_id: 'String',
+      google_id: 'String',
+      instagram_id: 'String',
+      picture_url: 'String',
+      cover_url: 'String',
+      fanpage_id: 'ObjectId',
+      metadata: 'Object',
+      created: {
+        type: Date,
+        "default": Date.now
+      },
+      show_birthday: {
+        type: Boolean,
+        "default": true
+      },
+      show_bio: {
+        type: Boolean,
+        "default": true
+      },
+      show_gender: {
+        type: Boolean,
+        "default": true
+      },
+      hidden: {
+        type: Boolean,
+        "default": false
+      },
+      random: {
+        type: [Number],
+        index: '2d',
+        "default": function() {
+          return [Math.random(), Math.random()];
+        }
+      }
+    });
+    facebookUserSchema = mongoose.Schema({
+      id: 'String',
+      user_id: 'String',
+      name: 'String',
+      first_name: 'String',
+      middle_name: 'String',
+      last_name: 'String',
+      username: {
+        type: String,
+        lowercase: true,
+        trim: true
+      },
+      url: 'String',
+      gender: 'String',
+      email: 'String',
+      emails: {
+        type: Array
+      },
+      quotes: 'String',
+      bio: 'String',
+      birthday: 'Date',
+      locale: 'String',
+      timezone: 'String',
+      verified: 'String',
+      metadata: 'Object',
+      accessToken: 'String',
+      created: {
+        type: Date,
+        "default": Date.now
+      }
+    });
+    googleUserSchema = mongoose.Schema({
+      id: 'String',
+      user_id: 'String',
+      name: 'String',
+      first_name: 'String',
+      last_name: 'String',
+      emails: {
+        type: Array
+      },
+      metadata: 'Object',
+      identifier: 'String',
+      created: {
+        type: Date,
+        "default": Date.now
+      }
+    });
+    instagramUserSchema = mongoose.Schema({
+      id: 'String',
+      user_id: 'String',
+      name: 'String',
+      first_name: 'String',
+      last_name: 'String',
+      bio: 'String',
+      website: 'String',
+      profile_picture: 'String',
+      metadata: 'Object',
+      accessToken: 'String',
+      created: {
+        type: Date,
+        "default": Date.now
+      }
+    });
+    userSchema.plugin(passportLocalMongoose);
     mongoose.model('chat_messages', chatMessageSchema);
     mongoose.model('topics', topicSchema);
     mongoose.model('rooms', roomSchema);
@@ -96,6 +233,10 @@
     mongoose.model('twitter_tags', twitterTagSchema);
     mongoose.model('open_graph_tags', openGraphTagSchema);
     mongoose.model('api_logs', apiLogSchema);
+    mongoose.model('users', userSchema);
+    mongoose.model('facebook_user_data', facebookUserSchema);
+    mongoose.model('instagram_user_data', instagramUserSchema);
+    mongoose.model('google_user_data', googleUserSchema);
     db = mongoose.connection;
     db.on('error', function(error) {
       return console.log('Mongodb returned error: %s', error);
